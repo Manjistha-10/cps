dfb030b7fe720aa8d4a13c24278e541607ba5731
Manjistha Bidkar <130024878+Manjistha-10@users.noreply.github.com>
2025-06-13 23:20:03 +0530

    Match topic from extracted text to the sample data dependency graph saved as excel file

--- project-assessment/pdf-parser/src/topicIdentifier.ts
+++ project-assessment/pdf-parser/src/topicIdentifier.ts
@@ -1,4 +1,4 @@
-// Developed by Manjistha Bidkar
+// Developed by Manjistha Bidkar 
 
 import { extractTextFromPDF } from './utils/pdfExtractor';
 import { loadConceptsFromExcel, identifyConcepts } from './utils/matchTopics';
